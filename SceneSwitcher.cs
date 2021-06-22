9297c4e08ad1b19385e8258c7945d9af4f75232a
Syed Huzaifa Ali <80565640+syedhuzaifaali660@users.noreply.github.com>
2021-06-22 22:47:28 +0500

    Add files via upload

---
+++ SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+public class SceneSwitcher : MonoBehaviour
+{
+   [MenuItem("EditorScript/Load MainMenu Scene %#m")]
+   static void OpenSceneFunction()
+    {
+        Debug.Log("Ye le khail");
+        //UnityEditor.SceneManagement.OpenSceneMode("path of scene",openscene)
+        EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity", OpenSceneMode.Single);
+    }
+}
